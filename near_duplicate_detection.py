54871c646461ab9117ba32608f9f21096db85e48
JacobLoe <Jcbl423@gmail.com>
2019-03-29 01:52:47 +0100

    made an update for the compared distances

--- near_duplicate_detection.py
+++ near_duplicate_detection.py
@@ -24,11 +24,15 @@ from keras.applications.nasnet import NASNetMobile
 
 from keras.preprocessing import image
 
-from keras.applications.vgg16 import preprocess_input
-from keras.applications.vgg19 import preprocess_input
-from keras.applications.resnet50 import preprocess_input
-from keras.applications.inception_v3 import preprocess_input
-from keras.applications.inception_resnet_v2 import preprocess_input
+from keras.applications.vgg16 import preprocess_input as pivgg16
+from keras.applications.vgg19 import preprocess_input as pivgg19
+from keras.applications.resnet50 import preprocess_input as pires
+from keras.applications.inception_v3 import preprocess_input as piin3
+from keras.applications.inception_resnet_v2 import preprocess_input as piin2
+
+from keras.applications.densenet import preprocess_input as pidense
+
+from keras.applications.nasnet import preprocess_input as pinas
 
 from scipy.spatial.distance import euclidean
 #####################################################
@@ -98,10 +102,10 @@ def read_video(video,start_ms,end_ms):
     return frames
 #########################################################################################################
 # uses a given model and an image(numpy array) and returns its features
-def extract_features(model,frame):
+def extract_features(model,frame,pi):
 
     frame=np.expand_dims(frame, axis=0)
-    frame=preprocess_input(frame)
+    frame=pi(frame)#preprocess_input(frame)
     features = model.predict(frame)
 
     return features
@@ -121,133 +125,150 @@ if __name__ == "__main__":
     print('done')
     #################################################
     # models
-    print('load models')                                                                                                        #lowest distance
-#    vgg16_model = VGG16(weights='imagenet', include_top=False,pooling='max') #pooling applies global max pooling to last layer #580
-#    vgg19_model = VGG19(weights='imagenet', include_top=False,pooling='max')                                                   #876
-#    ResNet50_model = ResNet50(weights='imagenet', include_top=False,pooling='max')                                             #226
-#    InceptionV3_model = InceptionV3(weights='imagenet', include_top=False,pooling='max')                                       #14353
-#    InceptionResNetV2_model = InceptionResNetV2(weights='imagenet', include_top=False,pooling='max')                           #35538
-#    DenseNet121_model = DenseNet121(weights='imagenet', include_top=False,pooling='max')                                       #71
-#    DenseNet169_model = DenseNet169(weights='imagenet', include_top=False,pooling='max')                                       #98
-#    DenseNet201_model = DenseNet201(weights='imagenet', include_top=False,pooling='max')                                       #91
+    print('load models')
+                                                                                                                                #lowest distance
+#    NASNetMobile_model = NASNetMobile(weights='imagenet', include_top=False,pooling='max',input_shape=(224,224,3))             #64
+#    DenseNet121_model = DenseNet121(weights='imagenet', include_top=False,pooling='max')                                       #80
+#    DenseNet169_model = DenseNet169(weights='imagenet', include_top=False,pooling='max')                                       #102
+#    DenseNet201_model = DenseNet201(weights='imagenet', include_top=False,pooling='max')                                       #83
+#    vgg16_model = VGG16(weights='imagenet', include_top=False,pooling='max') #pooling applies global max pooling to last layer #448
+#    vgg19_model = VGG19(weights='imagenet', include_top=False,pooling='max')                                                   #404
+#    ResNet50_model = ResNet50(weights='imagenet', include_top=False,pooling='max')                                             #251
+#    InceptionV3_model = InceptionV3(weights='imagenet', include_top=False,pooling='max')                                       #107
+#    InceptionResNetV2_model = InceptionResNetV2(weights='imagenet', include_top=False,pooling='max')                           #77
 #    NASNetLarge_model = NASNetLarge(weights='imagenet', include_top=False,pooling='max',input_shape=(331,331,3))               #152       #needs images as 331x331
-    NASNetMobile_model = NASNetMobile(weights='imagenet', include_top=False,pooling='max',input_shape=(224,224,3))             #64
+
     print('done')
     #####################################################################################################
 '''    print('search for image in video')
-    target_frame_features = extract_features(NASNetLarge_model,x)
+    target_frame_features = extract_features(NASNetMobile_model,x, pinas)
 
     distances=[]
 
     for begin_ms,end_ms in tqdm(shots):
         frame_list=read_video(args.video_path,begin_ms,end_ms)
         for frame in frame_list:
-            features = extract_features(NASNetLarge_model, frame)
+            features = extract_features(NASNetMobile_model, frame, pinas)
+            distances.append(euclidean(features,target_frame_features))
+
+    #print(distances)
+    print('\n','NASNetMobile ',min(distances))
+    #####################################################################################################
+    target_frame_features = extract_features(DenseNet121_model,x, pidense)
+
+    distances=[]
+
+    for begin_ms,end_ms in tqdm(shots):
+        frame_list=read_video(args.video_path,begin_ms,end_ms)
+        for frame in frame_list:
+            features = extract_features(DenseNet121_model, frame, pidense)
+            distances.append(euclidean(features,target_frame_features))
+
+    #print(distances)
+    print('\n','DenseNet121 ',min(distances))
+    #####################################################################################################
+    target_frame_features = extract_features(DenseNet169_model,x, pidense)
+
+    distances=[]
+
+    for begin_ms,end_ms in tqdm(shots):
+        frame_list=read_video(args.video_path,begin_ms,end_ms)
+        for frame in frame_list:
+            features = extract_features(DenseNet169_model, frame, pidense)
+            distances.append(euclidean(features,target_frame_features))
+
+    #print(distances)
+    print('\n','DenseNet169 ',min(distances))
+    #####################################################################################################
+    target_frame_features = extract_features(DenseNet201_model,x, pidense)
+
+    distances=[]
+
+    for begin_ms,end_ms in tqdm(shots):
+        frame_list=read_video(args.video_path,begin_ms,end_ms)
+        for frame in frame_list:
+            features = extract_features(DenseNet201_model, frame, pidense)
+            distances.append(euclidean(features,target_frame_features))
+
+    #print(distances)
+    print('\n','DenseNet201 ',min(distances))
+    #####################################################################################################
+    target_frame_features = extract_features(vgg16_model,x, pivgg16)
+
+    distances=[]
+
+    for begin_ms,end_ms in tqdm(shots):
+        frame_list=read_video(args.video_path,begin_ms,end_ms)
+        for frame in frame_list:
+            features = extract_features(vgg16_model, frame, pivgg16)
+            distances.append(euclidean(features,target_frame_features))
+
+    #print(distances)
+    print('\n','vgg16 ',min(distances))
+    #####################################################################################################
+    target_frame_features = extract_features(vgg19_model,x, pivgg19)
+
+    distances=[]
+
+    for begin_ms,end_ms in tqdm(shots):
+        frame_list=read_video(args.video_path,begin_ms,end_ms)
+        for frame in frame_list:
+            features = extract_features(vgg19_model, frame, pivgg19)
+            distances.append(euclidean(features,target_frame_features))
+
+    #print(distances)
+    print('\n','vgg19 ',min(distances))
+    #####################################################################################################
+    target_frame_features = extract_features(ResNet50_model,x, pires)
+
+    distances=[]
+
+    for begin_ms,end_ms in tqdm(shots):
+        frame_list=read_video(args.video_path,begin_ms,end_ms)
+        for frame in frame_list:
+            features = extract_features(ResNet50_model, frame, pires)
+            distances.append(euclidean(features,target_frame_features))
+
+    #print(distances)
+    print('\n','ResNet50_model ',min(distances))
+    #####################################################################################################
+    target_frame_features = extract_features(InceptionV3_model,x, piin3)
+
+    distances=[]
+
+    for begin_ms,end_ms in tqdm(shots):
+        frame_list=read_video(args.video_path,begin_ms,end_ms)
+        for frame in frame_list:
+            features = extract_features(InceptionV3_model, frame, piin3)
+            distances.append(euclidean(features,target_frame_features))
+
+    #print(distances)
+    print('\n','InceptionV3_model ',min(distances))
+    #####################################################################################################
+    target_frame_features = extract_features(InceptionResNetV2_model,x, piin2)
+
+    distances=[]
+
+    for begin_ms,end_ms in tqdm(shots):
+        frame_list=read_video(args.video_path,begin_ms,end_ms)
+        for frame in frame_list:
+            features = extract_features(InceptionResNetV2_model, frame, piin2)
+            distances.append(euclidean(features,target_frame_features))
+
+    #print(distances)
+    print('\n','InceptionResNetV2_model ',min(distances))'''
+    #####################################################################################################
+'''    print('search for image in video')
+    target_frame_features = extract_features(NASNetLarge_model,x, pinas)
+
+    distances=[]
+
+    for begin_ms,end_ms in tqdm(shots):
+        frame_list=read_video(args.video_path,begin_ms,end_ms)
+        for frame in frame_list:
+            features = extract_features(NASNetLarge_model, frame, pinas)
             distances.append(euclidean(features,target_frame_features))
 
     #print(distances)
     print('\n','NASNetLarge ',min(distances))'''
     #####################################################################################################
-'''    target_frame_features = extract_features(DenseNet121_model,x)
-
-    distances=[]
-
-    for begin_ms,end_ms in tqdm(shots):
-        frame_list=read_video(args.video_path,begin_ms,end_ms)
-        for frame in frame_list:
-            features = extract_features(DenseNet121_model, frame)
-            distances.append(euclidean(features,target_frame_features))
-
-    #print(distances)
-    print('\n','DenseNet121 ',min(distances))'''
-    #####################################################################################################
-'''    target_frame_features = extract_features(DenseNet169_model,x)
-
-    distances=[]
-
-    for begin_ms,end_ms in tqdm(shots):
-        frame_list=read_video(args.video_path,begin_ms,end_ms)
-        for frame in frame_list:
-            features = extract_features(DenseNet169_model, frame)
-            distances.append(euclidean(features,target_frame_features))
-
-    #print(distances)
-    print('\n','DenseNet169 ',min(distances))'''
-    #####################################################################################################
-'''    target_frame_features = extract_features(DenseNet201_model,x)
-
-    distances=[]
-
-    for begin_ms,end_ms in tqdm(shots):
-        frame_list=read_video(args.video_path,begin_ms,end_ms)
-        for frame in frame_list:
-            features = extract_features(DenseNet201_model, frame)
-            distances.append(euclidean(features,target_frame_features))
-
-    #print(distances)
-    print('\n','DenseNet201 ',min(distances))'''
-    #####################################################################################################
-'''    target_frame_features = extract_features(vgg16_model,x)
-
-    distances=[]
-
-    for begin_ms,end_ms in tqdm(shots):
-        frame_list=read_video(args.video_path,begin_ms,end_ms)
-        for frame in frame_list:
-            features = extract_features(vgg16_model, frame)
-            distances.append(euclidean(features,target_frame_features))
-
-    #print(distances)
-    print('\n','vgg16 ',min(distances))'''
-    #####################################################################################################
-'''    target_frame_features = extract_features(vgg19_model,x)
-
-    distances=[]
-
-    for begin_ms,end_ms in tqdm(shots):
-        frame_list=read_video(args.video_path,begin_ms,end_ms)
-        for frame in frame_list:
-            features = extract_features(vgg19_model, frame)
-            distances.append(euclidean(features,target_frame_features))
-
-    #print(distances)
-    print('\n','vgg19 ',min(distances))'''
-    #####################################################################################################
-'''    target_frame_features = extract_features(ResNet50_model,x)
-
-    distances=[]
-
-    for begin_ms,end_ms in tqdm(shots):
-        frame_list=read_video(args.video_path,begin_ms,end_ms)
-        for frame in frame_list:
-            features = extract_features(ResNet50_model, frame)
-            distances.append(euclidean(features,target_frame_features))
-
-    #print(distances)
-    print('\n','ResNet50_model ',min(distances))'''
-    #####################################################################################################
-'''    target_frame_features = extract_features(InceptionV3_model,x)
-
-    distances=[]
-
-    for begin_ms,end_ms in tqdm(shots):
-        frame_list=read_video(args.video_path,begin_ms,end_ms)
-        for frame in frame_list:
-            features = extract_features(InceptionV3_model, frame)
-            distances.append(euclidean(features,target_frame_features))
-
-    #print(distances)
-    print('\n','InceptionV3_model ',min(distances))'''
-    #####################################################################################################
-'''    target_frame_features = extract_features(InceptionResNetV2_model,x)
-
-    distances=[]
-
-    for begin_ms,end_ms in tqdm(shots):
-        frame_list=read_video(args.video_path,begin_ms,end_ms)
-        for frame in frame_list:
-            features = extract_features(InceptionResNetV2_model, frame)
-            distances.append(euclidean(features,target_frame_features))
-
-    #print(distances)
-    print('\n','InceptionResNetV2_model ',min(distances))'''
