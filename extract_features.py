291a02caf794910dfd20eaac452570c6ec34e7b2
JacobLoe <Jcbl423@gmail.com>
2019-07-18 01:32:56 +0200

    small fixes

--- extract_features.py
+++ extract_features.py
@@ -4,72 +4,76 @@ import argparse
 import numpy as np
 from tqdm import tqdm
 import glob
-import shutil
 
 from keras.applications.inception_resnet_v2 import InceptionResNetV2
 from keras.applications.inception_resnet_v2 import preprocess_input
 from keras.models import Model
 
-from keras.preprocessing.image import ImageDataGenerator, DirectoryIterator
+#from keras.preprocessing.image import ImageDataGenerator, DirectoryIterator
 #################################################################
 # uses a given model and an image(numpy array) and returns its features
+
+
 def extract_features(model, frame):
 
     frame = np.expand_dims(frame, axis=0)
     frame = preprocess_input(frame)
     features = model.predict(frame)
-    features = features/np.linalg.norm(features) #normalize length of feature vector
+    features = features/np.linalg.norm(features)  # normalize length of feature vector
 
     return features
 ###################################################
+
+
 def load_model():
-   print('load model')
-   model = InceptionResNetV2(weights='imagenet',input_shape=(299,299,3))
-   model = Model(inputs=model.input,output=model.get_layer('avg_pool').output)
-   print('done')
-   return model
+    print('load model')
+    model = InceptionResNetV2(weights='imagenet',input_shape=(299, 299, 3))
+    model = Model(inputs=model.input,output=model.get_layer('avg_pool').output)
+    print('done')
+    return model
 ####################################################
+
+
 def main(features_path):
 
-   model_name = 'features_InceptionResNetV2_avgpoolLayer'
+    model_name = 'features_InceptionResNetV2_avgpoolLayer'
 
-   images = os.path.join(features_path,'*/*/frames/*/*.png')
-   list_images_path = glob.glob(images) #get the list of videos in videos_dir
-   cp = os.path.commonprefix(list_images_path) #get the common dir between paths found with glob
+    images = os.path.join(features_path, '*/*/frames/*/*.png')
+    list_images_path = glob.glob(images)  # get the list of videos in videos_dir
+    cp = os.path.commonprefix(list_images_path)  # get the common dir between paths found with glob
    
-   list_features_path = [os.path.split(                # split of the shots folder
+    list_features_path = [os.path.split(                # split of the shots folder
                          os.path.split(                
                          os.path.split(                # split of the image-file name
-                         os.path.join(features_path,os.path.relpath(p,cp))
+                         os.path.join(features_path, os.path.relpath(p, cp))
                          )[0])[0])[0]
                          for p in list_images_path]
 
-   #model=load_model()
+    model = load_model()
 
-   idg = ImageDataGenerator()
-   di = DirectoryIterator(features_path,idg,batch_size=32,class_mode = None, shuffle = False)
-   #print(di.next())
+    #idg = ImageDataGenerator()
+    #di = DirectoryIterator(features_path,idg,batch_size=32,class_mode = None, shuffle = False)
+    #print(di.next())
 
+    for i_path, f_path in tqdm(zip(list_images_path, list_features_path), total=len(list_images_path)):
 
-'''   for i_path,f_path in tqdm(zip(list_images_path,list_features_path),total=len(list_images_path)):
+        print(f_path)
+        feature_name = os.path.split(i_path)[1][:-4]  # get the name of the image, remove the file extension
+        shot = os.path.split(os.path.split(i_path)[0])[1]  # get the name of the shot for the image
 
-       print(f_path)
-       feature_name = os.path.split(i_path)[1][:-4] #get the name of the image, remove the file extension
-       shot = os.path.split(os.path.split(i_path)[0])[1] #get the name of the shot for the image
-
-       fp = os.path.join(f_path,model_name,shot)
-       if not os.path.isdir(fp): #create the directory to save the features
-          os.makedirs(fp)
+        fp = os.path.join(f_path, model_name, shot)
+        if not os.path.isdir(fp):  # create the directory to save the features
+           os.makedirs(fp)
        
-       frame = cv2.imread(i_path) #read the image from disc
-       feature = extract_features(model,frame) #run the model on the image
-       path = os.path.join(fp,feature_name) #specify the path to which the feature is saved, the name is the same as the image (w/o the file-extension)
-       np.save(path,feature) #save the feature to disc'''
+        frame = cv2.imread(i_path)  # read the image from disc
+        feature = extract_features(model, frame)  # run the model on the image
+        path = os.path.join(fp, feature_name)  # specify the path to which the feature is saved, the name is the same as the image (w/o the file-extension)
+        np.save(path, feature)  # save the feature to disc
 #########################################################################################################
 if __name__ == "__main__":
 
-   parser = argparse.ArgumentParser()
-   parser.add_argument("features_dir",help="the directory where the feature-vectors are to be stored, for example 'features'")
-   args=parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("features_dir", help="the directory where the feature-vectors are to be stored, for example 'features'")
+    args = parser.parse_args()
 
-   main(args.features_dir)
+    main(args.features_dir)
