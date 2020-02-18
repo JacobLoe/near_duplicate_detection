d23777750a26ae62a655fec612b0ff86ff97fa12
JacobLoe <Jcbl423@gmail.com>
2020-02-19 00:18:10 +0100

    added trim_threshold argument to extract_images

--- copy_frames.py
+++ copy_frames.py
@@ -6,6 +6,9 @@ cf_path = 'static/features_videos/movies/Ferguson_Charles_Inside_Job/frames_crop
 ff_path = 'static/features_videos/movies/Ferguson_Charles_Inside_Job/full_frames'
 fc_path = 'static/features_videos/movies/Ferguson_Charles_Inside_Job/frames_comparison'
 
+if not os.path.isdir(fc_path):
+    os.makedirs(fc_path)
+
 for shot in tqdm(os.listdir(cf_path)):
     if not shot == '.done':
         o = os.path.join(cf_path, shot, shot)
