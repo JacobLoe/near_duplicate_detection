11078175dc9589229707508ebb7a490b3a5b3689
JacobLoe <Jcbl423@gmail.com>
2021-02-13 14:53:50 +0100

    moved functions into utils.py

--- ndd/utils.py
+++ ndd/utils.py
@@ -1,6 +1,19 @@
-# adopted from https://stackoverflow.com/a/56254114
+# https://stackoverflow.com/questions/10615901/trim-whitespace-using-pil/10616717#10616717
+from PIL import Image, ImageChops
 import subprocess
 import json
+
+
+def trim(im, threshold):
+    bg = Image.new(im.mode, im.size, im.getpixel((0, 0)))
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -threshold)
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop(bbox), bbox
+
+
+# adopted from https://stackoverflow.com/a/56254114
 def get_aspect_ratios(video_file):
     cmd = 'ffprobe -i "{}" -v quiet -print_format json -show_format -show_streams'.format(video_file)
     jsonstr = subprocess.check_output(cmd, shell=True, encoding='utf-8')
